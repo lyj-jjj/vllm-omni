6bce427f6db294f4e1153aab9d76a73248ad71cf
lyj-jjj <liuyingjun5@huawei.com>
2026-04-23 19:02:56 +0800

    add RMSNormVAE
    Signed-off-by: lyj-jjj <liuyingjun5@huawei.com>

--- vllm_omni/diffusion/models/wan2_2/patch_diffusers.py
+++ vllm_omni/diffusion/models/wan2_2/patch_diffusers.py
@@ -1,39 +1,14 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 
-from __future__ import annotations
-import torch
+import sys
 
-from torch import nn
-from vllm_omni.platforms import current_omni_platform
+from vllm_omni.diffusion.layers.rope import RMSNormVAE
+
 
 def patch_wan_rms_norm():
-    '''Replace small operators with fused operators'''
+    """Patch diffusers WanRMS_norm implementation with RMSNormVAE."""
 
-    if not current_omni_platform.is_npu():
-        return
-
-    import torch_npu
-    class WanRMS_norm(nn.Module):
-        def __init__(self, dim: int, channel_first: bool = True, images: bool = True, bias: bool = False) -> None:
-            super().__init__()
-            broadcastable_dims = (1, 1, 1) if not images else (1, 1)
-            shape = (dim, *broadcastable_dims) if channel_first else (dim,)
-            self.channel_first = channel_first
-            self.scale = dim ** 0.5
-            self.gamma = nn.Parameter(torch.ones(shape))
-            self.gamma_new = None
-            self.bias = nn.Parameter(torch.zeros(shape)) if bias else 0.0
-
-        def forward(self, x):
-            x = x.transpose(1, -1)
-            if self.gamma_new is None:
-                self.gamma_new = self.gamma.transpose(0, -1).reshape(-1)
-            x_out = torch_npu.npu_rms_norm(x, self.gamma_new, epsilon=1e-6)
-            x_out = x_out[0].transpose(1, -1)
-            return x_out
-
-    import sys
     for module_name, module in sys.modules.items():
         if hasattr(module, 'WanRMS_norm'):
-            setattr(module, 'WanRMS_norm', WanRMS_norm)
+            setattr(module, 'WanRMS_norm', RMSNormVAE)
